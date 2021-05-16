e87656749f1ef4b90afe0736fcb43186a009fee6
FL45H404 <70277710+FL45H404@users.noreply.github.com>
2021-05-16 20:59:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+   res.setHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
   next();
 });
 app.use("/",employeeleaveRoute);
