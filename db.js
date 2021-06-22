e1785dfb626a39b205b2c62327847a9daabdf4fb
FL45H404 <vipulwaghamode194@gmail.com>
2021-06-22 13:06:30 +0530

    cors chages

--- db.js
+++ db.js
@@ -2,8 +2,6 @@
 const mysql = require('mysql');
 //console.log("hii");
 //console.log(process.env.DATABASE_URL+"dburl");
-// const db = new Pool({
-//connectionString: 'postgres://isvfxhjtvulgxc:21c91a20eeab3d0f830a27a94393d2ba07d8d80d67304df45322e1b9266520ca@ec2-174-129-199-54.compute-1.amazonaws.com:5432/ddkmpapq4co4ko',
 var db = mysql.createPool({
   connectionLimit:100,
   queueLimit: 100,
@@ -17,20 +15,6 @@ var db = mysql.createPool({
   debug:false
 });
 
-// const db = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: process.env.DATABASE_URL ? true : false
-// })
-
-//client.connect();
-//pg.defaults.ssl = ;
-//var connection = "postgres://postgres:postgres@35.200.222.98:5432/lms-dev";
-//var connection = "postgres://postgres:Garima@123@localhost:5432/postgres";
-//var connection = "postgres://postgres:Garima@123@localhost:5432/hrlms";
-//var connection = "postgres://postgres:p@ssw0rd@localhost:5432/HRLMS_Local";
-//var connection="postgres://isvfxhjtvulgxc:21c91a20eeab3d0f830a27a94393d2ba07d8d80d67304df45322e1b9266520ca@ec2-174-129-199-54.compute-1.amazonaws.com:5432/ddkmpapq4co4ko";
-//var db=new pg.Client(connection);
-//var db=pg.connect();
 db.getConnection((err) => {
     if (err) {
         console.log(err)
--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-   res.setHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  //  res.setHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
   next();
 });
 app.use("/",employeeleaveRoute);
